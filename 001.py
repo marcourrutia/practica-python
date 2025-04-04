2a3d8ccc7c08952d0c5d3f0285d800520b0e30a9
marcourrutia <m.urrutia1991@gmail.com>
2025-04-04 20:07:54 -0300

    exercices 1

--- 001.py
+++ 001.py
@@ -1,3 +1,5 @@
-print("hola","todos","como","están", sep = "-")
+""" print("hola","todos","como","están", sep = "-")
 print("esto se imprime", end = " ")
-print("en una linea")
+print("en una linea") """
+
+print(round(3.5))
